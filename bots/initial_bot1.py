2c735c5d674dcbdae062a625ce75ad33570e3e49
Jiya <jiyasingla26@gmail.com>
2026-01-31 16:02:44 -0500

    yay

--- bots/initial_bot1.py
+++ bots/initial_bot1.py
@@ -9,6 +9,58 @@ from item import Pan, Plate, Food
 
 
 
+class MonteCarloTreeSearchNode():
+    def __init__(self, state, parent=None, parent_action=None):
+        self.state = state
+        self.parent = parent
+        self.parent_action = parent_action
+        self.children = []
+        self._number_of_visits = 0
+        self._results = defaultdict(int)
+        self._results[1] = 0
+        self._results[-1] = 0
+        self._untried_actions = None
+        self._untried_actions = self.untried_actions()
+        return
+    def untried_actions(self):
+        self._untried_actions = self.state.get_legal_actions()
+        return self._untried_actions
+    
+    def q(self):
+        wins = self._results[1]
+        loses = self._results[-1]
+        return wins - loses
+    def n(self):
+        return self._number_of_visits
+    def expand(self):
+	
+        action = self._untried_actions.pop()
+        next_state = self.state.move(action)
+        child_node = MonteCarloTreeSearchNode(
+            next_state, parent=self, parent_action=action)
+
+        self.children.append(child_node)
+        return child_node   
+    def is_terminal_node(self):
+        return self.state.is_game_over()
+    def rollout(self):
+        current_rollout_state = self.state
+        
+        while not current_rollout_state.is_game_over():
+            
+            possible_moves = current_rollout_state.get_legal_actions()
+            
+            action = self.rollout_policy(possible_moves)
+            current_rollout_state = current_rollout_state.move(action)
+        return current_rollout_state.game_result()  
+    def backpropagate(self, result):
+        self._number_of_visits += 1.
+        self._results[result] += 1.
+        if self.parent:
+            self.parent.backpropagate(result)  
+
+    def is_fully_expanded(self):
+        return len(self._untried_actions) == 0 
 
 class BotPlayer:
     def __init__(self, map_copy):
