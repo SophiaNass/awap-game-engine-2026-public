533294e34ef624955918d2831d5a8f61edd279eb
Mak Surendran <msurendr@andrew.cmu.edu>
2026-02-01 13:52:06 -0500

    dotty poos

--- bots/duo_noodle_bot.py
+++ bots/duo_noodle_bot.py
@@ -521,60 +521,59 @@ class BotPlayer:
 
                     case BotActions.CHOP:
                         # handle chop
-                        ...
+                        
                         controller.chop(bot_id, curr_move[2][1][0], curr_move[2][1][1])
 
                     case BotActions.BUY_PLATE:
                         # handle buy plate
                         controller.buy(bot_id, curr_move[2][1][0], curr_move[2][1][1], FoodType.PLATE)
-                        ...
 
                     case BotActions.BUY_PAN:
                         # handle buy pan
                         controller.buy(bot_id, curr_move[2][1][0], curr_move[2][1][1], FoodType.PAN)
-                        ...
+                        
 
                     case BotActions.BUY_EGG:
                         # handle buy egg
                         controller.buy(bot_id, curr_move[2][1][0], curr_move[2][1][1], FoodType.EGG)
-                        ...
+                        
 
                     case BotActions.BUY_ONION:
                         # handle buy onion
                         controller.buy(bot_id, curr_move[2][1][0], curr_move[2][1][1], FoodType.ONION)
-                        ...
+                        
 
                     case BotActions.BUY_MEAT:
                         # handle buy meat
                         controller.buy(bot_id, curr_move[2][1][0], curr_move[2][1][1], FoodType.MEAT)
-                        ...
+                        
 
                     case BotActions.BUY_NOODLES:
                         # handle buy noodles
                         controller.buy(bot_id, curr_move[2][1][0], curr_move[2][1][1], FoodType.NOODLES)
-                        ...
+                        
 
                     case BotActions.BUY_SAUCE:
                         # handle buy sauce
                         controller.buy(bot_id, curr_move[2][1][0], curr_move[2][1][1], FoodType.SAUCE)
-                        ...
+                        
 
                     case BotActions.PICKUP:
                         controller.pickup(bot_id, curr_move[2][1][0], curr_move[2][1][1])
-                        ...
+                        
 
                     case BotActions.PLACE_ITEM:
                         controller.place(bot_id, curr_move[2][1][0], curr_move[2][1][0])
-                        ...
+                        
 
                     case BotActions.TRASH:
                         controller.trash(bot_id, curr_move[2][1][0], curr_move[2][1][0])
 
-                        ...
+                        
 
                     case BotActions.TAKE_FROM_COUNTER:
                         controller.pickup(bot_id, curr_move[2][1][0], curr_move[2][1][1])
-                        ...
+                        
 
                     case BotActions.TAKE_FROM_PAN:
                         # handle take from pan
